23aae63713941845eef88ffa1bba5300e7612c6b
GwendalBesnard <gwendal.besnard@etu.univ-amu.fr>
2021-10-11 10:40:27 +0200

    struc site

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+    include 'php/utils.inc.php'; 
+?>
+<?php 
+    start_page ('Vanestarre');
+?>
+
+<div class="Bloc">
+    <border>
+        <a class="Logo" href="http://besnard.alwaysdata.net/"><img class="logo" src="images/Vanestarre.png" height="60px" width="110px" ></a>
+    </border>
+    <div class="Publi">
+        <h1 class="name">Vanestarre</h1>
+
+    </div>
+</div>
+
+<?php 
+    end_page();
+?>

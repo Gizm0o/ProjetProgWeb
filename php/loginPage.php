9a59688ef437eabbaee2f363941d6d520bdd0ad5
LoLeon <72595171+LoLeon@users.noreply.github.com>
2021-10-13 11:56:25 +0200

    petit ajout / correction

--- php/loginPage.php
+++ php/loginPage.php
@@ -4,20 +4,22 @@ start_page('Login');
 ?>
 
 <body>
-<form action="inc/login.inc.php" method="post" class="formLog">
-    <input name="pseudo" type="text" placeholder="Pseudo/Mail">
-    <input name="mdp" type="password" placeholder="Mot de Passe">
-    <button type="submit" name="submit">Se Connecter</button>
-</form>
-
+    <div class="formLog">
+        <form action="inc/login.inc.php" method="post" class="formLog">
+            <input class="field" name="pseudo" type="text" placeholder="Pseudo/Mail">
+            <input class="field" name="mdp" type="password" placeholder="Mot de Passe">
+            <button class="submit" type="submit" name="submit">Se Connecter</button>
+        </form>
+    </div>
 </body>
+
 <?php
 if(isset($_GET['error'])) {
     if ($_GET['error'] == 'emptyinput') {
-        echo '<p> Vous n\'avez pas tous rempli </p>';
+        echo '<p class="error"> Vous n\'avez pas tous rempli </p>';
     }
     if ($_GET['error'] == 'login') {
-        echo '<p> Les identifiants ne sont pas bon</p>';
+        echo '<p class="error"> Les identifiants ne sont pas bon</p>';
     }
 }
 
